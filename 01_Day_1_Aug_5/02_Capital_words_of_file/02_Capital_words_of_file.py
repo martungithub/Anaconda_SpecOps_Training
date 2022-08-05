f27be607a66ce0957fe855d9a20c6d484c58a58d
Martun Davitavyan <107114511+martungithub@users.noreply.github.com>
2022-08-06 02:48:43 +0400

    Day_1

--- 01_Day_1_Aug_5/02_Capital_words_of_file/02_Capital_words_of_file.py
+++ 01_Day_1_Aug_5/02_Capital_words_of_file/02_Capital_words_of_file.py
@@ -3,11 +3,6 @@ modified = open("modified.txt", "w")
 modified_word = ""
 for line in text:
     for word in line.split(" "):
-        if len(word) >= 3:
-            modified.write(word[:2] + word[2].upper() + word[3:] + " ")
-        elif len(word) == 3:
-            modified.write(word[:1] + word[2].upper() + " ")
-        else:
-            modified.write(word + " ")
+        modified.write(word[0].upper() + word[1:] + " ")
 text.close()
 modified.close()
