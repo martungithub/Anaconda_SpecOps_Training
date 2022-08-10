d3779d775997ab83f9c00f8b88091af562d9d081
martun <martun.davitavyan@mail.ru>
2022-08-10 13:52:24 +0400

    Day 3

--- 03_Day_3_Aug_7/01_Sum_of_squares.py
+++ 03_Day_3_Aug_7/01_Sum_of_squares.py
@@ -1,1 +1,7 @@
-print("hello")
+sum_of_square = 0
+sum_square = 0
+for i in range(0, 101):
+    sum_square += i
+    sum_of_square += i * i
+
+print("Difference: ", sum_square * sum_of_square - sum_of_square)
